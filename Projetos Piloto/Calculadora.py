435b35cd1d71026b9b6c5f19e655a6225aa2ebc8
Luis Gustavo Sales <adm.luissales@gmail.com>
2025-10-21 00:37:50 -0300

    Atualizações de atividades

--- Projetos Piloto/Calculadora.py
+++ Projetos Piloto/Calculadora.py
@@ -1,41 +1,44 @@
 
-
-def continuacao (n1,n2,operacao):
-       
-
-
-
 def resultado(n1,n2,operacao):
        if operacao == '+':
-              return f'{n1+n2}'
+              operacao = n1+n2
        elif operacao == '-':
-              return f'{n1-n2}'
+              operacao = n1-n2
        elif operacao == '/':
-              return f'{n1/n2}'
+              operacao = n1/n2
        elif operacao == '*':
-              return f'{n1*n2}'
+              operacao = n1*n2
        elif operacao == '^':
               return f'{pow(n1,n2)}'
-       elif operacao == 'raiz':
+       elif operacao == 'r':
               return f'{n1**(1/n2)}'
+       elif operacao == '=':
+              return ''
        else:
-              return SyntaxError
+              return None
        
+def menu():
+       operadores = {
+    '+': 'Somar',
+    '-': 'Subtrair',
+    '/': 'Divisão',
+    '*': 'Multiplicar',
+    '^': 'Potência',
+    'r': 'Raiz'}
+       print("MENU DA CALCULADORA")
+       print("Opções para a calculadora:")
+       for operador,obg in operadores.items():
+              print(f'{operador.ljust(2)} -> {obg.center(1)}')
 
-while True:
-        n1 = float(input('Digite um número:'))
-        operacao=str(input("Digite a operação:"))
-        if operacao == "=":
-                print()
-                break
-        else:
-            n2 = float(input("Digite o segundo número:"))
-            
-        
+menu()
+result = 0
+n1 = float(input('Digite um número:'))
+operacao=str(input("Digite a operação:"))
+n2 = float(input("Digite o segundo número:"))
+while operacao:
 
-        
-
-
-
-
-
+resultado_1 = resultado(n1,n2,operacao)
+if operacao != "=":
+       operacao = str(input('Digite a operação (= para sair):'))
+       n2_loop = float(input('Digite o próximo número:'))
+       resultado_loop =  resultado()
